c8057986604278f9793089b791bc42c089527b19
Chinmay Pareek <chinmaypareek@Chinmays-MacBook-Air.local>
2026-04-29 16:07:55 +0530

    Updates

---
+++ server/assets/_tanstack-start-manifest_v-Ds_yef9B.js
@@ -0,0 +1,4 @@
+const tsrStartManifest = () => ({ routes: { __root__: { filePath: "/Users/chinmaypareek/Downloads/arise-physiotherapy/src/routes/__root.tsx", children: ["/"], assets: void 0, preloads: ["/arisephysio/assets/index-Ca_j16V9.js"] }, "/": { filePath: "/Users/chinmaypareek/Downloads/arise-physiotherapy/src/routes/index.tsx", children: void 0, assets: void 0, preloads: ["/arisephysio/assets/index-DgYCaG6G.js"] } }, clientEntry: "/arisephysio/assets/index-Ca_j16V9.js" });
+export {
+  tsrStartManifest
+};
